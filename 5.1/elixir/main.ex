14ee33ce1d5c12f94b5685013225374aa41f16a5
MickeJagekrans <micke.jagekrans@gmail.com>
2023-12-05 19:26:09 +0100

    Day 5 part 1

---
+++ 5.1/elixir/main.ex
@@ -0,0 +1,86 @@
+lines = File.read!("../../5.input")
+
+# lines =
+# "seeds: 79 14 55 13
+
+# seed-to-soil map:
+# 50 98 2
+# 52 50 48
+
+# soil-to-fertilizer map:
+# 0 15 37
+# 37 52 2
+# 39 0 15
+
+# fertilizer-to-water map:
+# 49 53 8
+# 0 11 42
+# 42 0 7
+# 57 7 4
+
+# water-to-light map:
+# 88 18 7
+# 18 25 70
+
+# light-to-temperature map:
+# 45 77 23
+# 81 45 19
+# 68 64 13
+
+# temperature-to-humidity map:
+# 0 69 1
+# 1 0 69
+
+# humidity-to-location map:
+# 60 56 37
+# 56 93 4"
+
+[seeds | maps] = lines |> String.split("\n\n")
+
+# Convert seeds to integers
+seeds =
+  seeds
+  |> String.split(": ")
+  |> Enum.at(1)
+  |> String.split(" ")
+  |> Enum.map(&String.to_integer/1)
+
+defmodule Almanac do
+  def parse_map(line) do
+    line
+    |> String.split(" ")
+    |> Enum.map(&String.to_integer/1)
+    |> parse_map_values()
+  end
+
+  defp parse_map_values([dest, source, range]) do
+    {source, source + range - 1, dest - source}
+  end
+
+  def walk(value, []), do: value
+  def walk(value, [map | maps]), do: walk(value + find_offset(map, value), maps)
+
+  defp find_offset(map, value) do
+    found_value =
+      map |> Enum.find(fn {start, stop, _} -> value in start..stop end)
+
+    case found_value do
+      {_, _, offset} -> offset
+      _ -> 0
+    end
+  end
+end
+
+maps =
+  maps
+  |> Enum.map(fn map ->
+    map
+    |> String.split("\n")
+    |> tl()
+    |> Enum.map(&Almanac.parse_map/1)
+  end)
+
+seeds
+|> Enum.map(&Almanac.walk(&1, maps))
+|> Enum.min()
+|> IO.inspect()
