e771dc4f5db94162dcf62fada698469de1873d1a
MickeJagekrans <micke.jagekrans@gmail.com>
2023-12-11 17:08:54 +0100

    day 9 part 2

---
+++ 9.2/scala/main.scala
@@ -0,0 +1,30 @@
+import scala.annotation.tailrec
+import scala.util.chaining.*
+
+val getHistory: List[Int] => List[Int] = 
+  _.sliding(2).collect { case Seq(a, b) => b - a }.toList
+
+def getHistories(prevList: List[Int]): List[List[Int]] =
+  @tailrec def _getHistories(acc: List[List[Int]]): List[List[Int]] =
+    val history = getHistory(acc.head)
+    val newList = history +: acc
+
+    if history.forall(_ == 0) then newList
+    else _getHistories(newList)
+
+  _getHistories(prevList :: Nil)
+
+val splitToInts: String => List[Int] = _.split(" ").map(_.toInt).toList
+val reverseNestedLists: List[List[Int]] => List[List[Int]] = _.map(_.reverse)
+val sumListHeads: (List[Int], List[Int]) => List[Int] = (a, b) => (b.head - a.head) +: b
+
+@main def main(): Unit =
+  // Read all lines from file ../../9.input
+  val lines = io.Source.fromFile("../../9.input").getLines.toArray
+  val result =
+    lines
+      .map(splitToInts andThen getHistories)
+      .map(_.reduce(sumListHeads).head)
+      .reduce(_ + _)
+
+  println(result)
